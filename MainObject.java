43269897564593bd5a378b29b135d2169f594a94
Cheng <weijc@e0b55fe8e1f4.ant.amazon.com>
2021-04-09 02:05:50 +0800

    make dynamic drag/drop

--- MainObject.java
+++ MainObject.java
@@ -21,6 +21,7 @@ public abstract class MainObject extends JButton{
 	public boolean selected = false;
 	int connection_port_width = 5;
 	GUI main_GUI;
+	int drag_start_position_x, drag_start_position_y, drag_end_position_x, drag_end_position_y;
 
 	public MainObject(GUI m, int x, int y, int width, int heigh) {
 		super();
@@ -48,7 +49,7 @@ public abstract class MainObject extends JButton{
 	class CanvasMouseEvent extends MouseAdapter implements MouseInputListener{
 		
 		MainObject current_obj;
-		int start_position_x, start_position_y, end_position_x, end_position_y;
+		
 		
 		public CanvasMouseEvent(MainObject obj) {
 			this.current_obj = obj;
@@ -63,15 +64,17 @@ public abstract class MainObject extends JButton{
 		@Override
 		public void mouseDragged(MouseEvent e) {
 			// TODO Auto-generated method stub
-//			System.out.println("mouseDragged X:"+e.getX()+" Y:"+e.getY());
-//			repaint();
+			
+			for(MainObject obj: main_GUI.selected_object) {
+				obj.setLocation(obj.getLocation().x + e.getX()-current_obj.drag_start_position_x, obj.getLocation().y +e.getY()-current_obj.drag_start_position_y);
+			}
 			
 		}
 
 		@Override
 		public void mouseMoved(MouseEvent e) {
 			// TODO Auto-generated method stub
-			System.out.println("mouseMoved X:"+e.getX()+" Y:"+e.getY());
+//			System.out.println("mouseMoved X:"+e.getX()+" Y:"+e.getY());
 			
 		}
 
@@ -79,34 +82,30 @@ public abstract class MainObject extends JButton{
 		public void mousePressed(MouseEvent e) {
 			// TODO Auto-generated method stub
 			System.out.println("mousePressed X:"+e.getX()+" Y:"+e.getY());
-			this.start_position_x = e.getX();
-			this.start_position_y = e.getY();
 			
+			//設定拖拉物件的起始相對位置
+			current_obj.drag_start_position_x = e.getX();
+			current_obj.drag_start_position_y = e.getY();
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent e) {
 			// TODO Auto-generated method stub
 			System.out.println("mouseReleased X:"+e.getX()+" Y:"+e.getY());
-			this.end_position_x = e.getX();
-			this.end_position_y = e.getY();
-			for(MainObject obj: main_GUI.selected_object) {
-				obj.setLocation(obj.getLocation().x + this.end_position_x-this.start_position_x, obj.getLocation().y +this.end_position_y-this.start_position_y);
-			}
 			
 		}
 
 		@Override
 		public void mouseEntered(MouseEvent e) {
 			// TODO Auto-generated method stub
-			System.out.println("mouseEnter X:"+e.getX()+" Y:"+e.getY());
+//			System.out.println("mouseEnter X:"+e.getX()+" Y:"+e.getY());
 			
 		}
 
 		@Override
 		public void mouseExited(MouseEvent e) {
 			// TODO Auto-generated method stub
-			System.out.println("mouseExited X:"+e.getX()+" Y:"+e.getY());
+//			System.out.println("mouseExited X:"+e.getX()+" Y:"+e.getY());
 			
 		}
 		
